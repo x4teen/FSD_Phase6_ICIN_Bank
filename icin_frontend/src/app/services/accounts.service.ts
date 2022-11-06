3e7729f594dad3d588389825dd4ef1302c650738
x4teen <x4teen@yahoo.com>
2022-11-06 14:13:49 -0500

    docs

--- icin_frontend/src/app/services/accounts.service.ts
+++ icin_frontend/src/app/services/accounts.service.ts
@@ -2,28 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountsService {
-
   private url: string = 'http://localhost:8080/accounts';
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   //Get balance
-  public getAccountBalance(accountId:number) {
+  public getAccountBalance(accountId: number) {
     return this.httpClient.get(`${this.url}/balance/${accountId}`);
   }
 
-  public deposit(depositBody:any) {
+  public deposit(depositBody: any) {
     return this.httpClient.post(`${this.url}/deposit`, depositBody);
   }
 
-  public withdrawal(withdrawalBody:any) {
+  public withdrawal(withdrawalBody: any) {
     return this.httpClient.post(`${this.url}/withdrawal`, withdrawalBody);
   }
 
-  public transfer(transferBody:any) {
+  public transfer(transferBody: any) {
     return this.httpClient.post(`${this.url}/transfer`, transferBody);
   }
-  
 }
